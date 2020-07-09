8dd7729680f7635aa47157dbe5eed1b42a5bfc50
Crowdbotics <team@crowdbotics.com>
2020-07-09 05:37:59 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -8,6 +8,14 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    binary = models.BinaryField(null=True, blank=True,)
+    bolean = models.BooleanField(null=True, blank=True,)
+    char = models.CharField(null=True, blank=True, max_length=256,)
+    date = models.DateField(null=True, blank=True,)
+    datetime = models.DateTimeField(null=True, blank=True,)
+    decimal = models.DecimalField(
+        null=True, blank=True, max_digits=30, decimal_places=10,
+    )
 
     def __str__(self):
         return self.title
@@ -23,6 +31,20 @@ class CustomText(models.Model):
 
 class HomePage(models.Model):
     body = models.TextField()
+    duration = models.DurationField(null=True, blank=True,)
+    email = models.EmailField(null=True, blank=True, max_length=254,)
+    integer = models.IntegerField(null=True, blank=True,)
+    float = models.FloatField(null=True, blank=True,)
+    foreign = models.ForeignKey(
+        "users.User",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="homepage_foreign",
+    )
+    test = models.ManyToManyField(
+        "home.CustomText", blank=True, related_name="homepage_test",
+    )
 
     @property
     def api(self):
--- backend/users/models.py
+++ backend/users/models.py
@@ -7,6 +7,13 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
+    ipAddress = models.GenericIPAddressField(
+        null=True, blank=True, protocol="both", unpack_ipv4=False,
+    )
+    positiveinteger = models.PositiveIntegerField(null=True, blank=True,)
+    slug = models.SlugField(null=True, blank=True, max_length=50,)
+    text = models.TextField(null=True, blank=True,)
+    time = models.TimeField(null=True, blank=True,)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
